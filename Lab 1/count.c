79b719aa764d07021b77816f7b4dd7a2ef5b6444
Spencer Goles <sgoles@scu.edu>
2019-01-09 12:22:13 -0800

    First

---
+++ Lab 1/count.c
@@ -0,0 +1,12 @@
+/*********************************************************************/ 
+/*  Spencer Goles    COEN 12         9 Jan 2019                      */
+/*  Program Function: To count number of words in a text file        */
+/*********************************************************************/ 
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argu, int[] argv)
+{
+    return 1;
+}
