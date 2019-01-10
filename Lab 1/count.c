81348b0920223e02249b66d9db2129516d15b8b5
Spencer Goles <sgoles@scu.edu>
2019-01-09 17:22:13 -0800

    Final

--- Lab 1/count.c
+++ Lab 1/count.c
@@ -3,10 +3,29 @@
 /*  Program Function: To count number of words in a text file        */
 /*********************************************************************/ 
 
+//Pre-processors
 #include <stdio.h>
 #include <string.h>
+#define MAX_WORD_LENGTH 30
 
-int main(int argu, int[] argv)
+//Main Function
+int main(int argc, char *argv[ ])
 {
-    return 1;
+    FILE *fp;
+    char str[MAX_WORD_LENGTH];
+    int count = 0; 
+    //Check to see if the file is included
+    if (argc == 1)
+    {
+        printf("The name of the file is missing!\n");
+        return 1;
+    }
+    //Opens the file to scan
+    fp = fopen(argv[1], "r");
+    //Loop to count words in the file
+    while(fscanf(fp, "%s", str) == 1) count++;
+    fclose(fp);
+    printf("\nWord Count: %d\n", count); 
+    return 0;
 }
+
