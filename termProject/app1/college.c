9e4c1565d25801fb0608452d041f69c504bda7ef
Spencer Goles <sgoles@scu.edu>
2019-03-12 20:29:40 -0700

    asdf

---
+++ termProject/app1/college.c
@@ -0,0 +1,6 @@
+/*
+*   Spencer Goles   COEN 12   12 March 2019
+*   Program: college.c      Project: app1 (Term Project)
+*   Description:
+*
+*/
---
+++ termProject/app1/dataset.c
@@ -0,0 +1,6 @@
+/*
+*   Spencer Goles   COEN 12   12 March 2019
+*   Program: dataset.c      Project: app1 (Term Project)
+*   Description:
+*
+*/
